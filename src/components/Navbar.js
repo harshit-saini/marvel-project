4c9f9cb13c630659b71a31f0b10de2196e641386
harshit-saini <harshitr.x.t.10@gmail.com>
2022-02-13 12:55:16 +0530

    made navabar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
+
+  const navigationRef = useRef();
+
+  const handleMenuBtn = () => {
+    console.log("menu btn clicked")
+  }
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bold bg-danger bg-gradient" style={{ height: "70px" }}>
+    <nav className="navbar navbar-expand-lg navbar-dark bold bg-danger bg-gradient" style={{ minHeight: "70px" }}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">MARVEL</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="/">MARVEL</Link>
+        <button onClick={handleMenuBtn} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContents" aria-controls="navContents" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navContents" >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              {/* <a className="nav-link" aria-current="page" href="#">Home</a> */}
+              <Link className="nav-link" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              {/* <a className="nav-link" href="#">Link</a> */}
+              <Link className="nav-link" to="/">Link</Link>
             </li>
             <li className="nav-item">
-              {/* <a className="nav-link disabled">Disabled</a> */}
+              <Link className="nav-link" to="/">Disabled</Link>
             </li>
           </ul>
         </div>
