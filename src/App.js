852ccb9470b51d6b02407ec509816b2293d05220
harshit-saini <harshitr.x.t.10@gmail.com>
2022-02-13 19:49:45 +0530

    added code splitting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React, { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/Home';
-import Comics from './pages/Comics';
-import Creators from './pages/Creators';
-import { Routes, Route } from 'react-router-dom';
+
+// lazy loading
+const Home = lazy(() => import("./pages/Home"));
+const Comics = lazy(() => import("./pages/Comics"));
+const Creators = lazy(() => import("./pages/Creators"));
+const CodeSplitCheck = lazy(() => import("./components/CodeSplitCheck"));
 
 
-
-
-function App() {
+const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/comics" element={<Comics />} />
-        <Route path="/creators" element={<Creators />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/comics" element={<Comics />} />
+          <Route path="/creators" element={<Creators />} />
+          <Route path="/split-check" element={<CodeSplitCheck />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
