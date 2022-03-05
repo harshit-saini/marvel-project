4c4c78ed686a082d937275e99fdbc6531043e9bc
harshit-saini <harshitr.x.t.10@gmail.com>
2022-03-05 23:04:59 +0530

    navbar themeswitch fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react';
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 import { ThemeContext } from '../contexts/ThemeContext'
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 
-const activeLink = {
-  borderBottom: "2px solid white",
-  color: "white",
-  fontWeight: "bold"
-}
-
 
 const CustomLink = ({ to, children, ...props }) => {
-
+  const activeLink = {
+    borderBottom: "2px solid white",
+    color: "white",
+    fontWeight: "bold",
+    display: "inline-block"
+  }
   // const resolvedPath = useResolvedPath(to)  -- i dont know whats its use
 
   const isMatch = useMatch(to)
@@ -24,48 +24,135 @@ const CustomLink = ({ to, children, ...props }) => {
   )
 }
 
+const renderThemeSwitch = (handleThemeChange, checked) => (
+  <div className="ms-md-2 me-md-1 px-md-2 d-flex justify-content-start justify-content-md-center align-items-center border-md-start" >
+    <FontAwesomeIcon style={{ color: "orange" }} icon={faSun} />
+    <div class="form-check form-switch ms-2 ms-2" >
+      <input class="form-check-input" type="checkbox" role="switch" onChange={handleThemeChange} value="" checked={checked} />
+    </div>
+    <FontAwesomeIcon style={{ color: "grey" }} icon={faMoon} />
+  </div>
+)
+
+const renderLinks = () => (
+  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+    <li className="nav-item">
+      <CustomLink className="nav-link" to="/">Home</CustomLink>
+    </li>
+    <li className="nav-item">
+      <CustomLink className="nav-link" to="/comics">Comics</CustomLink>
+    </li>
+    <li className="nav-item">
+      <CustomLink className="nav-link" to="/creators">Creators</CustomLink>
+    </li>
+  </ul>
+)
+
+const renderMenuBtn = () => (
+  <button className="navbar-toggler menuBtn" id="menuBtn" type="button" data-bs-toggle="collapse" data-bs-target="#navContents" aria-controls="navContents" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+)
+
+
 
 const Navbar = () => {
 
+  // const Nav = styled.nav`
 
-  const themeContext = useContext(ThemeContext)
+  //   display : flex;
+  //   flex-direction : column;
+
+  //   @media(min-width : 768px){
+  //     flex-direction: row;
+  //   }
+
+
+  //   .brand {
+  //     text-decoration : none;
+  //     color : white;
+  //     font-weight: bold;
+
+  //   }
+
+  //   .menuBtn{
+  //     margin-left : auto;
+  //     border : 2px solid white;
+  //     padding : 2px 4px;
+  //     border-radius : 3px;
+  //     color : white;
+  //     font-weight: 400;
+  //     display: inline-block;
+  //     cursor: pointer;
+  //     @media(min-width : 768px){
+  //       display: none;
+  //     }
+
+  //   }
+
+  //   #navContents{
+  //     margin-top: 20px;
+
+  //     ul{
+  //       display: flex;
+  //       flex-direction: column;
+  //       padding-left: 0;
+  //       margin-bottom: 0;
+  //       list-style: none;
+  //       @media(min-width : 768px){
+  //         flex-direction : row;
+  //       }
+  //     }
+
+  //     li {
+  //       margin-bottom : 10px;
+  //       a{
+  //         text-decoraion: none;
+  //         color : white;
+
+  //       }
+  //     }
+
+  //     @media(min-width : 768px){
+  //       flex-direction : row;
+  //     }
+
+
+  //   }
+
+
+  //   /* styled for opened menu */
+  //   position: fixed;
+  //   top : 0;
+  //   left : 0;
+  //   height : 100%;
+  //   width : 100%;
+  //   z-index : 10000;
+  //   padding : 20px;
 
 
 
 
-  const handleMenuBtn = () => {
-    console.log("menu btn clicked")
-  }
+
+
+
+
+
+
+  // `
+
+
+  const { isLightTheme, handleThemeChange } = useContext(ThemeContext)
+  const checked = isLightTheme ? false : true;
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bold bg-danger bg-gradient" style={{ minHeight: "70px" }}>
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-dark bold bg-danger bg-gradient">
+      <div class="container-fluid">
         <Link className="navbar-brand" to="/">MARVEL</Link>
-        <button onClick={handleMenuBtn} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContents" aria-controls="navContents" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        < div className="collapse navbar-collapse" id="navContents" >
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <CustomLink className="nav-link" to="/">Home</CustomLink>
-            </li>
-            <li className="nav-item">
-              <CustomLink className="nav-link" to="/comics">Comics</CustomLink>
-            </li>
-            <li className="nav-item">
-              <CustomLink className="nav-link" to="/creators">Creators</CustomLink>
-            </li>
-          </ul>
-          <div>
-
-            <div className="ms-md-2 me-md-1 px-md-2 d-flex justify-content-center align-items-center border-start" >
-              <FontAwesomeIcon icon={faSun} />
-              <div class="form-check form-switch ms-2 ms-2" >
-                <input class="form-check-input" type="checkbox" role="switch" onChange={themeContext.handleThemeChange} />
-              </div>
-              <FontAwesomeIcon icon={faMoon} />
-            </div>
-          </div>
+        {renderMenuBtn()}
+        <div className="collapse navbar-collapse" id="navContents">
+          {renderLinks()}
+          {renderThemeSwitch(handleThemeChange, checked)}
         </div>
       </div>
     </nav>
